69c1db65dff5c12838a3990dd599a6641826b354
BalajiSivakumar31 <163494131+BalajiSivakumar31@users.noreply.github.com>
2026-02-03 16:54:35 +0530

    Add AWS Secrets Manager integration
    Integrate AWS Secrets Manager to retrieve external service secrets.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,32 @@
+
+import {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} from "@aws-sdk/client-secrets-manager";
+
+const secret_name = "MyApp/ExternalService";
+
+const client = new SecretsManagerClient({
+  region: "ap-south-2",
+});
+
+let response;
+
+try {
+  response = await client.send(
+    new GetSecretValueCommand({
+      SecretId: secret_name,
+      VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+    })
+  );
+} catch (error) {
+  // For a list of exceptions thrown, see
+  // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+  throw error;
+}
+
+const secret = response.SecretString;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -23,3 +52,4 @@ app.UseAuthorization();
 app.MapRazorPages();
 
 app.Run();
+
